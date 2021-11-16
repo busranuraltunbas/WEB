8ace163a1d117db44c7ccd71e85494550584cc29
busranuraltunbas <altunbasbusranur@gmail.com>
2021-11-16 18:09:54 +0300

    ..

---
+++ JS/script.js
@@ -0,0 +1,68 @@
+//console.log("External");
+
+//let selam = "Hello";
+
+//console.log(selam);
+
+//selam = "Merhaba";
+
+//console.log(selam);
+
+//var deger = "Helloooo";
+
+//console.log(deger);
+
+//var deger = "Merhaba";
+
+//console.log(deger);
+
+function fonn(){
+    var deger = "text";
+    console.log(deger);
+}
+
+fonn();
+//console.log(deger);
+
+var a;
+console.log("deger", typeof(a));
+
+var b = null;
+console.log("deger", typeof(b));
+
+//let id = Symbol();
+
+var lesson = {
+    name : "Web",
+    number : 1
+};
+
+console.log("Object", lesson);
+console.log("Object", lesson.name);
+
+
+//Array
+
+var dizi = [1,2,3];
+console.log("dizi", typeof(dizi));
+console.log("item", dizi[1]);
+dizi[3] = 4;
+console.log(dizi);
+console.log(dizi.length);
+
+
+function hello(){
+    console.log("Function");
+}
+
+hello()
+
+
+var num =  true ;
+var ynum = Number(num);
+console.log(ynum);
+
+
+var str = 1;
+var ss = String(str);
+console.log(ss);
